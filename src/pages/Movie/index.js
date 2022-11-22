41ec7f52871450488d5e256ce07b00a941353686
juniormathias <juniormathiaas@gmail.com>
2022-11-22 14:13:49 -0300

    just i added a style for movie page also i referencied some links ex youtube one

--- src/pages/Movie/index.js
+++ src/pages/Movie/index.js
@@ -1,11 +1,13 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import api from '../../services/api'
+import './style.css';
 
 function Movie(){
     //getting the params from routes.js 
 
     const { id } = useParams();
+    const navigate = useNavigate();
     //gettint the movies also the loading page
     const [movie, setmovies] = useState({});
     const [loading, setLoading] = useState(true);
@@ -24,14 +26,15 @@ useEffect(()=>{
             setLoading(false);
         })
         .catch(()=>{
-
+            navigate("/", {replace:true});
+            return;
         })
     }
     loadMovie();
     return () => {
         
     }
-}, [])
+}, [navigate, id])
 if(loading){
     return(
         <div className='info-movie'>
@@ -43,6 +46,19 @@ if(loading){
         <div className='info-movie'>
             <h1>{movie.title}</h1>
             <img src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`} alt={movie.title} />
+
+            <h3>Synopsis</h3>
+            <span>{movie.overview}</span>
+            <strong>Ratings: {movie.vote_average} /10</strong>
+
+            <div className='buttons-area'>
+                <button>Save</button>
+                <button>
+                    <a target="_blank" rel="external" href={`https://youtube.com/results?search_query=${movie.title} Trailer`}>
+                    Official Trailer
+                    </a>
+                </button>
+            </div>
         </div>
     )
 }
